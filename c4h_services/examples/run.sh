0ed1dbf5ed5b108811ec7476b65ec063f12ec144
Dimitar Popov <jimpopov@gmail.com>
2025-01-15 17:04:47 +1100

    runner

---
+++ c4h_services/examples/run.sh
@@ -0,0 +1,2 @@
+~/src/apps/c4h master
+(venv) venv ❯  python c4h_services/examples/prefect_runner.py agent --agent semantic_iterator --config c4h_agents/examples/configs/iter_config_01.yml --log debug
